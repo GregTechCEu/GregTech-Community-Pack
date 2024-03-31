e3120c5d7ff45f0a8fd38a438bf433d93a6c9502
ursamina <143211829+ursamina@users.noreply.github.com>
2024-03-31 16:11:56 +0100

    oredict cleanup
    alphabetise files, add forestry storage blocks to oredict removals

--- groovy/postInit/oredict/additions.groovy
+++ groovy/postInit/oredict/additions.groovy
@@ -1,7 +1,3 @@
-
-// Gregtech
-ore('itemSilicon').add(item('gregtech:meta_plate', 99)) // GT Silicon Plate
-
 // AE2
 ore('gemFluix').add(item('appliedenergistics2:material', 7))
 
@@ -10,14 +6,17 @@ ore('gemZanite').add(item('aether:zanite_gemstone'))
 ore('gemIcestone').add(item('aether:icestone'))
 ore('gemAmbrosium').add(item('aether:ambrosium_shard'))
 
-// Midnight
-ore('gemEbonite').add(item('midnight:ebonys'))
-
 // Botania
 ore('gemDragonstone').add(item('botania:manaresource', 9))
 ore('blockManasteel').add(item('botania:storage'))
 ore('blockTerrasteel').add(item('botania:storage', 1))
 ore('blockElvenElementium').add(item('botania:storage', 2))
+
+// Midnight
+ore('gemEbonite').add(item('midnight:ebonys'))
+
+// Gregtech
+ore('itemSilicon').add(item('gregtech:meta_plate', 99)) // GT Silicon Plate
 
 // Thaumic Bases
 ore('ingotThauminite').add(item('thaumicbases:thauminite_ingot'))
